c0524afa15745bf225dcddf54ad8a90f2c264219
Walker Logan <walker.logan44@gmail.com>
2023-05-11 00:21:15 -0500

    entered sequalize on connection.js

---
+++ connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(error => console.error('Unable to connect to the database:', error));
+
+module.exports = sequelize;
