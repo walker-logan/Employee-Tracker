23c87376ad3409fe90255c744bdfcd04f4904656
Walker Logan <walker.logan44@gmail.com>
2023-05-11 01:19:25 -0500

    working on seeds

--- connection.js
+++ connection.js
@@ -10,8 +10,4 @@ const sequelize = new Sequelize({
   port: 3306,
 });
 
-sequelize.authenticate()
-  .then(() => console.log('Connection has been established successfully.'))
-  .catch(error => console.error('Unable to connect to the database:', error));
-
 module.exports = sequelize;
