af36dcde0282c2ea4aa5cd696b93db2b3002174c
Walker Logan <walker.logan44@gmail.com>
2023-05-23 21:39:30 -0500

    re-thinking the logic behind index.js

--- index.js
+++ index.js
@@ -1,15 +1,125 @@
-const db = require('./config/connection.js');
-const { displayTitleText } = require('./lib/specialText.js');
-const selectTask = require('./lib/selectTask.js');
+// Importing necessary dependencies and models
+const { Department, Role, Employee } = require("./Models");
+const sequelize = require("./connection");
+const inquirer = require("inquirer");
 
-// Connect to the database and handle any errors or start application
-db.connect(err => {
-    if (err) throw err;
-    init();
+// Syncing the database with created models
+sequelize.sync({ force: false }).then(() => {
+  options();
 });
 
-// Start the application, show title special-text and present task list prompt (using 'selectTask')
-const init = () => {
-    displayTitleText();
-    selectTask();
-};
+// Function to prompt the user with different options to navigate the database
+function options() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+        ],
+        name: "employeeTracker",
+      },
+    ])
+    .then((answer) => {
+      // Executes appropriate function based on user choice
+      const actionMap = {
+        "View All Departments": viewAllDepartments,
+        "View All Roles": viewAllRoles,
+        "View All Employees": viewAllEmployees,
+        "Add Department": addDepartment,
+        "Add Role": addRole,
+        "Add Employee": addEmployee,
+        "Update Employee Role": updateEmployeeRole,
+      };
+      actionMap[answer.employeeTracker]();
+    });
+}
+
+// --- VIEW FUNCTIONS ---
+
+// Function to view all departments
+async function viewAllDepartments() {
+  const departments = await Department.findAll({ raw: true });
+  console.table(departments);
+  options();
+}
+
+// Function to view all roles
+async function viewAllRoles() {
+  const roles = await Role.findAll({
+    raw: true,
+    include: [{ model: Department }],
+  });
+  console.table(roles);
+  options();
+}
+
+// Function to view all employees
+async function viewAllEmployees() {
+  const employees = await Employee.findAll({
+    raw: true,
+    include: [{ model: Role, include: [{ model: Department }] }],
+  });
+  console.table(employees);
+  options();
+}
+
+// --- ADD FUNCTIONS ---
+
+// Function to add a new department
+async function addDepartment() {
+  const department = await inquirer.prompt([
+    { type: "input", message: "What would you like to name the department?", name: "name" },
+  ]);
+  await Department.create(department);
+  options();
+}
+
+// Function to add a new role
+async function addRole() {
+  const departments = await Department.findAll({
+    attributes: [["id", "value"], ["name", "name"]],
+  });
+  const role = await inquirer.prompt([
+    { type: "input", message: "What is the name of the role?", name: "title" },
+    { type: "input", message: "What would you like the salary to be?", name: "salary" },
+    { type: "list", message: "What department would you like to add this new role to?", name: "department_id", choices: departments },
+  ]);
+  await Role.create(role);
+  options();
+}
+
+// Function to add a new employee
+async function addEmployee() {
+  const roles = await Role.findAll({ attributes: [["id", "value"], ["title", "name"]] });
+  const managers = await Employee.findAll({ attributes: [["id", "value"], ["first_name", "name"], ["last_name", "lastName"]] });
+  const employee = await inquirer.prompt([
+    { type: "input", message: "What is the first name of the new employee?", name: "first_name" },
+    { type: "input", message: "What is the last name of the new employee?", name: "last_name" },
+    { type: "list", message: "What is the role of the new employee?", name: "role_id", choices: roles },
+    { type: "list", message: "Who is the manager of the new employee?", name: "manager_id", choices: managers },
+  ]);
+  await Employee.create(employee);
+  options();
+}
+
+// --- UPDATE FUNCTION ---
+
+// Function to update the role of an existing employee
+async function updateEmployeeRole() {
+  const employees = await Employee.findAll({ attributes: [["id", "value"], ["first_name", "name"], ["last_name", "lastName"]] });
+  const roles = await Role.findAll({ attributes: [["id", "value"], ["title", "name"]] });
+  const update = await inquirer.prompt([
+    { type: "list", message: "Which employee's role do you want to update?", name: "id", choices: employees },
+    { type: "list", message: "What is the new role of the employee?", name: "role_id", choices: roles },
+  ]);
+  await Employee.update(update, { where: { id: update.id } });
+  options();
+}
