d4987f06e6f6d19cee92904ece7ba3f0b3ce768e
Walker Logan <walker.logan44@gmail.com>
2023-05-23 21:37:57 -0500

    re-thinking logic behind sql. i changed the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,4 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
+DROP DATABASE IF EXISTS employee_manager_db;
+CREATE DATABASE employee_manager_db;
 
-CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
-    INDEX (department_id)
-);
-
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    INDEX (role_id)
-);
+USE employee_manager_db;
